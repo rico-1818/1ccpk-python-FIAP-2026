cf439dc63d8e2865235821e730d44aaed62c2385
rico-1818 <risantos1818@gmail.com>
2026-03-16 09:53:24 -0300

    aula2

---
+++ aula02.py
@@ -0,0 +1,19 @@
+print("ola mundo")
+print(7*8) # concatenando strings
+print("7")
+# comentario em python utilizo #
+nome = "guanabara"
+idade = 25
+peso = 75.8
+
+print(idade, nome, peso)
+
+#input utilizado para receber dados do úsuario
+
+nome = input("digite seu nome: ")
+idade = input("digite sua idade: ")
+peso = input("digite seu peso: ")
+
+print(nome, idade, peso)
+
+ 
---
+++ desafio.py
@@ -0,0 +1,16 @@
+nome = input("qual é o seu nome: ")
+print("seja bem vindo", nome)
+
+# desafio2
+dia = input("qual e o seu dia de nascimento: ")
+mes = input("qual e o seu mês de nascimento:")
+ano = input("qual e o seu ano de nascimento:")
+
+print(dia, mes, ano)
+
+# desafio 3
+num1 = float(input("digite seu numero: "))
+num2 = float(input("digite seu numero: "))
+soma = num1 + num2
+
+print("o resultado da soma e", soma)
